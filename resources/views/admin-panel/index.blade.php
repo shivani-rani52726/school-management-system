38ed32e0444202a7f1eb8ab0b5992c237c421f79
Rajbir Shakya <arbrainentertainment@gmail.com>
2025-01-21 15:55:19 +0530

    fix: cdn remove link header only use vite

--- resources/views/admin-panel/index.blade.php
+++ resources/views/admin-panel/index.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <style>
+
+        </style>
+    @endif
 
 </head>
 <body class="bg-gray-50">
